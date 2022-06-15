aaf9741c9555267dd35277ff2879e7bc8149be90
Caike Machado <caikemachado@icloud.com>
2022-06-15 11:34:14 -0300

    aula 15 (adicionando controller de fornecedor, editando rotas e comentando blocos php)

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,19 @@
+<h3>Fornecedor</h3>
+
+{{'Teste de texto'}} {{-- maneira de exibir texto no blade --}}
+
+<?= 'Teste de texto' ?> {{-- maneira de exibir texto no blade --}}
+
+{{-- Comentário que será descartado pelo interpretador do blade --}}
+
+@php 
+
+    // Para comentários de uma linha
+
+    /*
+    Para comentários de multiplas linhas
+    */
+
+    echo 'Teste de texto' // maneira de comentar no php
+
+@endphp
