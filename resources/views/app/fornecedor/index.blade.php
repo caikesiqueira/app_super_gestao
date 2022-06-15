cc70afdea211aded604b3cc601ec98c3d91acac7
Caike Machado <caikemachado@icloud.com>
2022-06-15 12:13:34 -0300

    aula 16 (usando @if e @else no código)

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,19 +1,21 @@
 <h3>Fornecedor</h3>
 
-{{'Teste de texto'}} {{-- maneira de exibir texto no blade --}}
-
-<?= 'Teste de texto' ?> {{-- maneira de exibir texto no blade --}}
-
-{{-- Comentário que será descartado pelo interpretador do blade --}}
-
-@php 
-
-    // Para comentários de uma linha
-
-    /*
-    Para comentários de multiplas linhas
-    */
-
-    echo 'Teste de texto' // maneira de comentar no php
-
+@php
+/*
+    if () {
+        
+    } elseif () {
+        
+    } else {
+        
+    }
+*/
 @endphp
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores)> 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
