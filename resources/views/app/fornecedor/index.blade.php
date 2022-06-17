953459f7cc95156b2173b04d448936724a4fb77a
Caike Machado <caikemachado@icloud.com>
2022-06-17 13:00:48 -0300

    aula 21 (usando switch para alternar blocos de código que serão executados

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -15,9 +15,24 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{$fornecedores[1]['nome']}}
+    Fornecedor: {{$fornecedores[2]['nome']}}
 <br>
-    Status: {{$fornecedores[1]['status']}}
+    Status: {{$fornecedores[2]['status']}}
 <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido' }}
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
+<br>
+    Telefone: ({{ $fornecedores[2]['ddd'] ?? '' }}) {{ $fornecedores[2]['telefone'] ?? '' }}
+    @switch($fornecedores[2]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado
+    @endswitch
 @endisset 
