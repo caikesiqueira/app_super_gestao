2abc2e46d3deb6430204398927f6a8e0037c90e6
Caike Machado <caikemachado@icloud.com>
2022-06-17 09:11:42 -0300

    aula 18 (usando @isset para testar definição da variável)

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,22 +1,17 @@
 <h3>Fornecedor</h3>
 
 @php
-/*
-    if (!<condicao>) {} //enquanto executa se o retorno for "true"
-*/
+    /*
+    if (isset($variavel)) {} //retornar true se a variável estiver definida
+    */
 @endphp
 
-{{-- @unless executa se o retorno for "false" --}}
-
-Fornecedor: {{$fornecedores[0]['nome']}}
+@isset($fornecedores)
+    Fornecedor: {{$fornecedores[0]['nome']}}
 <br>
-Status: {{$fornecedores[0]['status']}}
+    Status: {{$fornecedores[0]['status']}}
 <br>
-@if(!($fornecedores[0]['status'] == 'S'))
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') <!-- Executa se o retorno da condição for "false" -->
-    Fornecedor inativo
-@endunless
-<br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{$fornecedores[0]['cnpj']}}
+    @endisset
+@endisset 
